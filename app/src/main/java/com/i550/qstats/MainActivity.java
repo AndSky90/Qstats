f19773c9d59ab727eee41274ed0d7bcc365ad419
AndSky90 <andsky90@gmail.com>
2018-12-05 00:58:36 +0300

    v0.24

--- app/src/main/java/com/i550/qstats/MainActivity.java
+++ app/src/main/java/com/i550/qstats/MainActivity.java
@@ -1,9 +1,8 @@
 package com.i550.qstats;
 
-import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
-import android.preference.PreferenceManager;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentManager;
@@ -11,14 +10,21 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
+
+import java.util.HashSet;
+import java.util.Set;
 
 //__________________________________________________________________________________________________
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "qStatser";
-    private static String profileName = "XF8ShaggyStoned";      //TODO потом удалить!
-    SharedPreferences sp;
+    public static final String PROFILE_NAMES_LIST = "namelist";
+    public static final String LAST_PROFILE_NAME= "last_profile_name";
+    private static String profileName;
+
+    private Set<String> profileNamesList;
+    private Boolean ADDED_NEW_PROFILE = false;
+    private SharedPreferences mSharedPreferences;
     private TabLayout tabLayout;
     private ViewPager viewPager;
     public ViewPagerAdapter vpa;
@@ -31,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
             R.drawable.ic_matches,
             R.drawable.ic_compare};
 
-    protected void configureTabLayout(){
+    protected void configureTabLayout() {
         for (int i = 0; i < tabLayout.getTabCount(); i++) {
             tabLayout.getTabAt(i).setIcon(tabIcons[i]);
             // tabLayout. TODO посмотреть разные методы, с иконкой намутить
@@ -53,28 +59,53 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onStop() {
+        if (ADDED_NEW_PROFILE) {
+            SharedPreferences.Editor editor = mSharedPreferences.edit();
+            editor.putStringSet(PROFILE_NAMES_LIST, profileNamesList).apply();
+            editor.putString(LAST_PROFILE_NAME, profileName).apply();
+            super.onStop();
+        }
+    }
+
+    protected void queryNewProfile() {
+        profileName = "XF8ShaggyStoned";
+        if(!profileNamesList.contains(profileName)) profileNamesList.add(profileName);
+        return;
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sp = PreferenceManager.getDefaultSharedPreferences(this);
-
 
         FragmentManager fm = getSupportFragmentManager();
         vpa = new ViewPagerAdapter(fm);
-
         viewPager = findViewById(R.id.viewpager);
         viewPager.setAdapter(vpa);
-        // viewPager.updateViewLayout();
         tabLayout = findViewById(R.id.tab_layout);
         tabLayout.setupWithViewPager(viewPager);
         configureTabLayout();
 
+
+        mSharedPreferences = getSharedPreferences(PROFILE_NAMES_LIST, Context.MODE_PRIVATE);
+        profileNamesList = new HashSet<>();
+        if (mSharedPreferences.contains(PROFILE_NAMES_LIST)) {
+            profileNamesList = mSharedPreferences.getStringSet(PROFILE_NAMES_LIST, new HashSet<String>());
+        }
+        mSharedPreferences = getSharedPreferences(LAST_PROFILE_NAME, Context.MODE_PRIVATE);
+        profileName = mSharedPreferences.getString(LAST_PROFILE_NAME,profileName);
+        if(profileName==null) queryNewProfile();
+        Log.i(TAG, "Prefs: PROFILE_NAMES_LIST: " + profileName + " LAST_PROFILE_NAME: " + profileNamesList );
+
+
         updateGlobalDataFromServer();
 
 
-
       /*
+
+
             headerLayout = findViewById(R.id.headerLayout);
             headerLayout.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -93,22 +124,16 @@ public class MainActivity extends AppCompatActivity {
 
     /*public void updateView(){
         viewPager.invalidate();
+        // viewPager.updateViewLayout();
     }*/
 
     public void updateGlobalDataFromServer() {
         new AsyncTaskGlobal().execute();
     }
-    /*
-TODO: public void getStartFragment(){
-        fm.beginTransaction();
-        fm.replace()
-        fragmentTransaction.addToBackStack(BACKSTACK_TAG)
-        // getLayoutInflater().inflate(R.layout.f_champions,null);
-*/
 
 //__________________________________________________________________________________________________
 
-     class AsyncTaskGlobal extends AsyncTask<Void, Void, Void> {
+    class AsyncTaskGlobal extends AsyncTask<Void, Void, Void> {
         @Override
         protected Void doInBackground(Void... voids) {
             NetQstatsWork background = new NetQstatsWork();
@@ -117,7 +142,7 @@ TODO: public void getStartFragment(){
             background.fetchLeaderBoard(getString(R.string.url_tdm_leads), false);
             background.fetchLeaderBoard(getString(R.string.url_duel_leads), true);
             background.fetchPlayerSummary(getString(R.string.url_player_summary), profileName);
-     //       background.fetchPlayerStats(getString(R.string.url_player_stats), profileName);
+            //       background.fetchPlayerStats(getString(R.string.url_player_stats), profileName);
 
             return null;
         }
@@ -147,7 +172,6 @@ TODO: public void getStartFragment(){
 
         }
     }*/
-
 
 
 }
